75728f51747c4f13f9ac5e0e373ad85b5f123845
jack0237 <99824627+jack0237@users.noreply.github.com>
2023-10-19 15:55:37 +0100

    .

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,7 +3,7 @@ import { HiHomeModern } from "react-icons/hi2";
 
 export default function footer() {
   return (
-    <footer className="bg-black w-full text-white lg:px-48 pt-7 pr-10 pb-1 pl-14 absolute bottom-0">
+    <footer className="bg-black w-full text-white lg:px-48 pt-7 pr-10 pb-1 pl-14 relative">
       <div className="container mx-auto grid grid-cols-1 md:grid-cols-3 gap-6">
         <div className="mb-4 md:mb-0">
           <h2 className="text-2xl font-semibold flex py-2">D <HiHomeModern className="h-auto"/> milis</h2>
