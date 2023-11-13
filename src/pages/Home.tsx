d36a0bde38df03bed7a825b0ffafa5cfa4a6f0df
ngdream <96875506+ngdream@users.noreply.github.com>
2023-11-13 20:21:57 +0100

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Footer from "../components/Footer/footer";
 import News_later from "../components/News_later/news_later";
 import Services from "../components/services/Services";
-import Header from "../components/header/header";
+import Header from "../components/Header/header";
 
 export default function Home():React.ReactElement {
   return (
