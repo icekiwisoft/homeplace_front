533f27ff51dd83646d941acfc65ed207e10c2744
jack0237 <99824627+jack0237@users.noreply.github.com>
2024-01-15 17:58:55 +0100

    .

---
+++ src/components/utilities/fourniture.js
@@ -0,0 +1,8 @@
+const dataList = [
+    { id: 1, name: 'Item 1', Number: '89', title:'total furniture ' },
+    { id: 2, name: 'Item 2', Number: '450', title:'total furniture ' },
+    { id: 3, name: 'Item 3', Number: '321', title:'total furniture ' },
+    { id: 3, name: 'Item 3', Number: '289', title:'total furniture ' },
+  ];
+  
+  export default dataList;
