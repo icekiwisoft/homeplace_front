00ac15a5002ab839ea137adf6cc18d5ef64b9b20
jack0237 <99824627+jack0237@users.noreply.github.com>
2024-01-15 18:04:32 +0100

    .

--- src/components/Dashboardcontent/Fourniture.tsx
+++ src/components/Dashboardcontent/Fourniture.tsx
@@ -8,7 +8,7 @@ const Fourniture = () => {
 
   return (
     <>
-      <div className="p-4 sm:ml-64">
+      <div className="p-4 ">
         <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
           <div className="grid grid-cols-4 gap-4 mb-4">
             {dataList.map((items) => (
