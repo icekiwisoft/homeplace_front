68e72d9b965ab7b2cf0bf32a508439e35124d53d
ngdream <96875506+ngdream@users.noreply.github.com>
2024-01-24 20:34:52 +0100

    f

--- src/components/Header/Hero/Hero.tsx
+++ src/components/Header/Hero/Hero.tsx
@@ -88,7 +88,7 @@ export default function Hero(): React.ReactElement {
                                 <div className=' absolute top-0 text-left sm:w-[90%] lg:w-[70%] w-full flex items-center justify-center h-full' >
                                     <div className=' pl-[25px] sm:pl-[10px]  md:pl-[75px] '>
                                         <h2 className='text-slidetitle sm:text-[4rem]  text-white my-5 '>{item.title}</h2>
-                                        <p className='text-slideparagraph my-7 sm:my-10 text-white'>
+                                        <p className='text-slideparagraph my-7 sm:my-10 text-gray-200'>
                                             {item.text}
                                         </p>
                                         <button className='transition cursor-pointer bg-[#fff] text-black font-bold px-5 py-2 my-2  rounded'>
--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -9,20 +9,28 @@ function Services():React.ReactElement {
       <div className='relative w-full md:py-[100px] p-[50px] flex justify-center items-center lg:px-[75px] xl:px-[120px] '>
         <div className='flex flex-col   gap-y-20 md:gap-[100px]'>
             <div className='flex justify-between  md:flex-row md:gap-x-20 gap-5 flex-col'>
-                <img className='h-auto md:w-[46%] w-full rounded-[20px] lg:m-w-[383px]' src={image2}/>
-                <div className='flex flex-col gap-5 items-start '>
-                    <h1 className='leading-none md:leading-tight w-3/4 '>Discover the house of your dreams</h1>
+              <div className='w-[483px] overflow-hidden rounded-[20px]'>
+              <img className=' min-w-full  min-h-full  ' src={image2}/>
+              </div>
+        
+                <div className='flex flex-col gap-5 items-start w-96 '>
+                    <h1  className='text-[2rem]  leading-none md:leading-tight font-bold'>Discover the house of your dreams</h1>
                     <span className='text-[1.3rem] text-gray-700'>Install the plugin and convert your designs to a responsive site.</span>
                     <button className='rounded-lg bg-gray-900  text-white p-4 w-48  '>View more</button>
                 </div>
             </div>
-            <div className='flex md:flex-row md:gap-x-20 gap-5 flex-col-reverse'>
-                <div className='flex flex-col gap-5 items-start w-1/2 lg:pr-[15%]'>
-                    <h1>Discover furnishings</h1>
+            <div className='flex md:flex-row md:gap-x-20 gap-5  justify-between flex-col-reverse'>
+                <div className='flex flex-col gap-5 items-start  w-96'>
+                    <h1 className='text-[2rem] font-bold'>Discover furnishings</h1>
                     <span className='text-[1.3rem] text-gray-700'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est perferendis.</span>
                     <button className='rounded-lg bg-gray-900  text-white p-4 w-48 '>View more</button>
                 </div>
-                <img className='h-auto md:w-[46%] w-full rounded-[20px] lg:m-w-[383px]' src={image}/>
+
+
+                <div className='w-[483px] overflow-hidden rounded-[20px]'>
+              <img className=' min-w-full  min-h-full object-fill ' src={image}/>
+              </div>
+         
             </div>
         </div>
       </div>
