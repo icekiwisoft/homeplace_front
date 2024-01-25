568903a3f51ca4123071ee8df39c65bdb813ebea
ngdream <96875506+ngdream@users.noreply.github.com>
2024-01-25 19:14:22 +0100

    add auth context

--- src/components/Dashboard/UserCard/UserCard.tsx
+++ src/components/Dashboard/UserCard/UserCard.tsx
@@ -1,28 +1,25 @@
 import React from "react";
 import { FaArrowUp } from "react-icons/fa";
 import { MdPerson } from "react-icons/md";
-
+import { Link } from "react-router-dom";
+import user  from '../../../assets/img/user.png';
 export default function UserCard()
 {
     return(
-        <div className="flex flex-col items-center justify-center h-28 rounded bg-gray-100 border shadow-md p-4 ">
+        <Link to={"/dashboard/announcers/1"} className="flex flex-col items-center justify-center h-28 rounded bg-gray-100 border shadow-md p-4 ">
         <div className='flex justify-between w-full'>
-            <div>
-                <p>Total Category </p>
-                <h2>5</h2>
+        <div className=" w-16 h-16">
+            <img src={user}  className=" object-fill"/>
             </div>
             <div>
-                <MdPerson className=' text-blue-700 text-[3.5rem]' />
+                <h2 className="font-semibold text-[1rem]"> domilis announcers</h2>
+                <time className="font-light text-[1rem]">22/23/2</time>
             </div>
-        </div>
-        <div className='flex items-center gap-1 w-full justify-between'>
-            <button className='flex items-center gap-1 bg-green-200 font-light text-[13px] rounded-full text-green-400 p-2'>
-                <FaArrowUp /> 100k
-            </button>
-            <p className=' text-gray-500 font-light'>Total furniture's</p>
+ 
         </div>
 
-    </div>
+
+    </Link>
 
     )
 }
