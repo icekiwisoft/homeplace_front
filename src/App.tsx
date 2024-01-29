d391910eee7708bb6c62c78b61be0bbc2e50cac9
jack0237 <99824627+jack0237@users.noreply.github.com>
2024-01-29 15:22:40 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,28 @@
-import React from 'react'
-import './App.scss'
-import Home from './pages/Home.tsx'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
-import Products from './pages/Products.tsx'
-import Dashboard from './pages/Dashboard.tsx'
-import Announcer from './pages/Dashboard/Announcer/Announcer.tsx'
-import Announcers from './pages/Dashboard/Announcers/Announcers.tsx'
-import Fourniture from './components/Dashboardcontent/Fourniture.tsx'
+import React from "react";
+import "./App.scss";
+import Home from "./pages/Home.tsx";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import Products from "./pages/Products.tsx";
+import Dashboard from "./pages/Dashboard.tsx";
+import Announcer from "./pages/Dashboard/Announcer/Announcer.tsx";
+import Announcers from "./pages/Dashboard/Announcers/Announcers.tsx";
+import Fourniture from "./components/Dashboardcontent/Fourniture.tsx";
 
 function App(): React.ReactElement {
-
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path='/' index Component={Home}/>
-<Route  path='announces' Component={Products} />
-          <Route path="/dashboard" Component={Dashboard}>
-            <Route  index  element={<Navigate to="announcers" replace/>} />
-            <Route path="announcers" element={<Announcers />} />
-            <Route path="furnitures" element={<Fourniture />} />
-            <Route path="announcers/:id" element={<Announcer/>}/>
-          </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" index Component={Home} />
+        <Route path="announces" Component={Products} />
+        <Route path="/dashboard" Component={Dashboard}>
+          <Route index element={<Navigate to="announcers" replace />} />
+          <Route path="announcers" element={<Announcers />} />
+          <Route path="furnitures" element={<Fourniture />} />
+          <Route path="announcers/:id" element={<Announcer />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Dashboardcontent/Fourniture.tsx
+++ src/components/Dashboardcontent/Fourniture.tsx
@@ -3,13 +3,13 @@ import { MdCategory } from "react-icons/md";
 import { FaArrowUp } from "react-icons/fa";
 import { data } from "autoprefixer";
 import dataList from "../utilities/fourniture";
+import { FaReact, FaAngular, FaVuejs, FaNode } from "react-icons/fa";
 
 const Fourniture = () => {
-
   return (
     <>
       <div className="p-4 ">
-        <div className="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
+        <div className="p-4 border-gray-200 rounded-lg ">
           <div className="grid grid-cols-4 gap-4 mb-4">
             {dataList.map((items) => (
               <div
@@ -18,60 +18,35 @@ const Fourniture = () => {
               >
                 <div className="flex justify-between w-full">
                   <div>
-                    <p></p>
+                    <p>{items.name}</p>
                     <h2>{items.Number}</h2>
                   </div>
                   <div>
-                    <MdCategory className=" text-blue-700 text-[3.5rem]" />
+                    <span className="material-symbols-outlined"></span>
                   </div>
                 </div>
                 <div className="flex items-center gap-1 w-full justify-between">
-                  <button className="flex items-center gap-1 bg-green-200 font-light text-[13px] rounded-full text-green-400 p-2">
+                  <button className="flex items-center gap-1 bg-green-200 font-light text-[13px] rounded-full {items.color}  p-2">
                     <FaArrowUp /> 100k
                   </button>
-                  <p className=" text-gray-500 font-light">Total furniture's</p>
+                  <p className=" text-gray-500 font-light">{items.title}</p>
                 </div>
               </div>
             ))}
           </div>
 
-          <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
-            <p className="text-2xl text-gray-400 dark:text-gray-500">
-              <svg
-                className="w-3.5 h-3.5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 18 18"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M9 1v16M1 9h16"
-                />
-              </svg>
-            </p>
+          <div className="flex items-center shadow-md justify-center h-auto mb-4 rounded bg-gray-100 p-8">
+            <div className="flex w-full justify-between">
+                <p className=" text-gray-500">Categories</p>
+                <button className=" rounded-full bg-green-300 p-2 px-4">Create</button>
+            </div>
+            <div></div>
           </div>
+
           <div className="grid grid-cols-2 gap-4 mb-4">
             <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
               <p className="text-2xl text-gray-400 dark:text-gray-500">
-                <svg
-                  className="w-3.5 h-3.5"
-                  aria-hidden="true"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 18 18"
-                >
-                  <path
-                    stroke="currentColor"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    stroke-width="2"
-                    d="M9 1v16M1 9h16"
-                  />
-                </svg>
+                
               </p>
             </div>
             <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
